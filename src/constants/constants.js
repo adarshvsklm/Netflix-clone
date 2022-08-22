f249acc2c9be91a39e76aaf0c1779aa167c55fa7
adarshvsklm <adarshvsklm@gmail.com>
2022-08-22 15:39:42 +0530

    final commit

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '13d6257c6c0b4a4fee2223520a07b0fb';
+export const imageUrl = 'https://image.tmdb.org/t/p/w500'
---
+++ src/urls.js
@@ -0,0 +1,5 @@
+import {API_KEY} from './constants/constants'
+export const originals = `discover/tv?api_key=${API_KEY}&with_networks=213`
+export const action = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
+export const comedy = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`
+export const horror = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`
